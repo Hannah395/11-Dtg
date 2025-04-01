d38558a28b67d8ddc6db81364956ac6d001956fb
Hannah395 <hannahsinclair@cromwell.school.nz>
2025-04-02 12:29:07 +1300

    Update Formula 1 Quiz.py
    I deleted my work and just added it in again because I made so many changed to make it work I got rid of the str next to the player_score = (0) to make my quiz run normally and then added the exit and fixed the if your under 15 it will tell you aren't old enough and finish the quiz there

--- Formula 1 Quiz.py
+++ Formula 1 Quiz.py
@@ -1,44 +1,52 @@
 # This code is a quiz.It asks you your name and age, also gives you your score as you go.
 import easygui
-import easygui
-player_score = str (0)
+player_score = (0)
 easygui.msgbox("Hi welcome to my quiz")
 easygui.msgbox("This quiz is about Formula one! Test your knowledge")
 player_age = easygui.integerbox("how old are you?")
-if player_age <= 18 and player_age >= 15:
+if player_age <= 18 and player_age >= 15:  
     easygui.msgbox("You are old enough for this quiz")
     easygui.msgbox("Welcome to the F1 quiz")
     player_name = easygui.enterbox("Hi What is your name?")
     easygui.msgbox(" Hi " + str(player_name) + " Welcome to the F1 quiz ")
-    easygui.msgbox (" Your score is " + player_score + " You will get your final score at the end ")
+    easygui.msgbox (" Your score is " + str(player_score) + " You will get your final score at the end ")
     easygui.msgbox("First question")
     answer = easygui.choicebox("What year did Daniel Riccardo lose his seat?",choices = ["2021", "2022", "2023","2024"] )
-if answer == "2024":
-    easygui.msgbox(" Great work! you got the answer right")
-    player_score += 1  
-else: 
-    easygui.msgbox("Sorry you got it wrong")
-        easygui.msgbox("Sorry you got it wrong")
-answer = easygui.choicebox("How long is Max Verstappen's contract ",choices = ["Until 2028", "Until 2027", "Until 2026", "Until 2025"])
-if answer == "Until 2028":
+    if answer == "2024":
+        easygui.msgbox(" Great work! you got the answer right")
+        player_score += 1  
+    else: 
+        easygui.msgbox("Sorry you got it wrong")  
+   
+    answer = easygui.choicebox("How long is Max Verstappen's contract ",choices = ["Until 2028", "Until 2027", "Until 2026", "Until 2025"])
+    if answer == "Until 2028":
         easygui.msgbox("Great work! you got it right")
         player_score +=1
-else:
+    else:
         easygui.msgbox("Sorry you got it wrong")
-answer = easygui.choicebox("Who is the richest formula 1 driver?",choices = ["Lewis", "Charles", "Yuki","Lance"])
-if answer == "Lewis":
+    
+    answer = easygui.choicebox("Who is the richest formula 1 driver?",choices = ["Lewis", "Charles", "Yuki","Lance"])  
+    if answer == "Lewis":
         easygui.msgbox("Great work! you got it right")
         player_score +=1
-else:
+    else:
         easygui.msgbox("Sorry you got it wrong")
-answer = easygui.choicebox("In what year did formula 1 begin",choices = ["1940","1950","1960","1970"])
-if answer == "1950":
+    
+    answer = easygui.choicebox("In what year did formula 1 begin",choices = ["1940","1950","1960","1970"])
+    if answer == "1950":
         easygui.msgbox("Great work! you got it right")
         player_score +=1
-else:
+    else:
         easygui.msgbox("Sorry you got it wrong")
-
+    
+    answer = easygui.choicebox("What two drivers are currently at Mclaren?",choices = ["Liam and Yuki","Pierre and Jack","Lando and Oscar","Max and Carlos"])
+    if answer == "Lando and Oscar":
+        easygui.msgbox ("Great work! you got it right")
+        player_score +=1
+    else:
+        easygui.msgbox("Sorry you got it wrong")
+easygui.msgbox(" Your final score is " + str(player_score)+ " points. Great job! ")
+easygui.msgbox("Thank you for taking this quiz")
+easygui.msgbox(" Bye "+ str (player_name) + "!")
 if player_age > 18 or player_age < 15:
     easygui.msgbox("You're not old enough to do this quiz.")    
-
-
