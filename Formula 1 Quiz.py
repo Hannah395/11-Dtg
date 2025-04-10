b930b75a6d6b7d4f981e77505dcaafc464e3d883
Hannah395 <hannahsinclair@cromwell.school.nz>
2025-04-10 15:03:10 +1200

    Update Formula 1 Quiz.py
    I added all my questions with loops I deleted the questions because it's annoying to put the loop in. I also added one comment

--- Formula 1 Quiz.py
+++ Formula 1 Quiz.py
@@ -20,41 +20,76 @@ if player_age <= 18 and player_age >= 15:
     easygui.msgbox(" Hi " + str(player_name) + " Welcome to the F1 quiz ")
     easygui.msgbox (" Your score is " + str(player_score) + " You will get your final score at the end ")
     #These are my 5 questions for my quiz each one has the answer in there and also has points to add if you get it right
+    # There is also a loop in my questions so if you get it wrong once you can have another try and get half a point if you get it right.
     easygui.msgbox("First question")
-    answer = easygui.choicebox("What year did Daniel Riccardo lose his seat?",choices = [Q_1] )
     if answer == "2024":
         easygui.msgbox(" Great work! you got the answer right")
-        player_score += 1  
-    else: 
-        easygui.msgbox("Sorry you got it wrong")  
-   
-    answer = easygui.choicebox("How long is Max Verstappen's contract ",choices = [Q_2])
+        player_score += 1
+    while  answer != "2024" and counter < 2:
+        easygui.msgbox("Sorry you got it wrong. Try again")  
+        counter += 2
+    answer = easygui.choicebox("What year did Daniel Riccardo lose his seat?",choices = Q_1 )
+    if answer == "2024":
+        easygui.msgbox(" Great work! you got the answer right")
+        player_score += 0.5
+    else:
+      easygui.msgbox("Sorry you got it wrong. You have no more trys left for this question") 
+    counter = 1
+    answer = easygui.choicebox("How long is Max Verstappen's contract ",choices = Q_2)
     if answer == "Until 2028":
         easygui.msgbox("Great work! you got it right")
         player_score +=1
-    else:
+    while  answer != "Until 2028" and counter < 2:
         easygui.msgbox("Sorry you got it wrong")
-    
-    answer = easygui.choicebox("Who is the richest formula 1 driver?",choices = [Q_3])  
+        counter += 2
+    answer = easygui.choicebox("How long is Max Verstappen's contract ",choices = Q_2)
+    if answer == "Until 2028":
+        easygui.msgbox("Great work! you got it right")
+        player_score += 0.5
+    else:
+        easygui.msgbox("Sorry you got it wrong. You have no more trys left for this question") 
+        counter = 1
+        answer = easygui.choicebox("Who is the richest formula 1 driver?",choices = Q_3)  
     if answer == "Lewis":
         easygui.msgbox("Great work! you got it right")
         player_score +=1
+    while  answer != "Lewis" and counter < 2:
+        easygui.msgbox ("Sorry you got it wrong")
+        counter += 2
+        answer = easygui.choicebox("Who is the richest formula 1 driver?",choices = Q_3) 
+    if answer == "Lewis":
+        easygui.msgbox("Great work! you got it right")   
+        player_score += 0.5
     else:
-        easygui.msgbox("Sorry you got it wrong")
-    
-    answer = easygui.choicebox("In what year did formula 1 begin",choices = [Q_4])
+        easygui.msgbox("Sorry you got it wrong. You have no more trys left for this question")
+    counter = 1
+    answer = easygui.choicebox("In what year did formula 1 begin",choices = Q_4)
     if answer == "1950":
         easygui.msgbox("Great work! you got it right")
         player_score +=1
-    else:
+    while answer != "1950" and counter < 2:
         easygui.msgbox("Sorry you got it wrong")
-    
-    answer = easygui.choicebox("What two drivers are currently at Mclaren?",choices = [Q_5])
+        counter += 2 
+        answer = easygui.choicebox("In what year did formula 1 begin",choices = Q_4)
+    if answer == "1950":
+        easygui.msgbox("Great work! you got it right")   
+        player_score += 0.5
+    else:
+        easygui.msgbox("Sorry you got it wrong. You have no more trys left for this question")
+    counter = 1
+    answer = easygui.choicebox("What two drivers are currently at Mclaren?",choices = Q_5)
     if answer == "Lando and Oscar":
         easygui.msgbox ("Great work! you got it right")
         player_score +=1
-    else:
+    while answer != " Lando and Oscar" and counter < 2:
         easygui.msgbox("Sorry you got it wrong")
+        counter += 2
+        answer = easygui.choicebox("What two drivers are currently at Mclaren?",choices = Q_5)
+    if answer == "Lando and Oscar":
+        easygui.msgbox("Great work! you got it right")
+        player_score += 0.5
+    else:
+        easygui.msgbox("Sorry you got it wrong. you have no trys left for this question")
     #This is my outro it gives you your final score and says goodbye with your name
     easygui.msgbox(" Your final score is " + str(player_score)+ " points. Great job! ")
     easygui.msgbox("Thank you for taking this quiz")
